051095caf40c11b2896f08fa7b1f6fa964397053
leeseonseonje <leeseonje9323@gmail.com>
2022-01-09 14:33:09 +0900

    m

---
+++ src/main/java/weather/clear/drivia/DriviaApplication.java
@@ -0,0 +1,13 @@
+package weather.clear.drivia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriviaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/weather/clear/drivia/DriviaApplicationTests.java
@@ -0,0 +1,13 @@
+package weather.clear.drivia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriviaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
